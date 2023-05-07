86074c6ef3cb15f072e309f0b823fde94b707526
Rain Zhu <rain1995zhu@gmail.com>
2023-05-07 15:11:04 -0700

    create user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
+  end
+  
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
 
-  def create
-  end 
-  
+  private
+    def user_params
+      params.require(:user).permit(:last_name, :first_name, :email, :password, :password_confirmation)
+    end
+
 end
