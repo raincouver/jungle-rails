07f64f40d3f5958d53dbc103d26b49ea1e1f7d20
Rain Zhu <rain1995zhu@gmail.com>
2023-05-08 16:59:46 -0700

    add authentication (class) method and rspec user test cases

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    # validation examples here
+  end
+
+  describe '.authenticate_with_credentials' do
+    # examples for this class method here
+  end
+
 end
