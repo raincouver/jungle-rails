352b67ecdb98ec5dfc47c69e162f62d4f61fc346
Rain Zhu <rain1995zhu@gmail.com>
2023-05-08 15:06:05 -0700

    pass validating test cases

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,6 +15,43 @@ RSpec.describe Product, type: :model do
           category: @category)
         expect(@product).to be_valid
      end
+
+     it 'should not save when name is nil' do
+      @product = Product.new(
+        name:nil,
+        price_cents: 9999,
+        quantity: 99,
+        category: @category)
+      expect(@product).to_not be_valid
+   end
+
+        it 'should not save when price_cents is nil' do
+        @product = Product.new(
+          name:'Peach',
+          price_cents: nil,
+          quantity: 99,
+          category: @category)
+        expect(@product).to_not be_valid
+     end
+
+     it 'should not save when quantity is nil' do
+      @product = Product.new(
+        name:'Peach',
+        price_cents: 9999,
+        quantity: nil,
+        category: @category)
+      expect(@product).to_not be_valid
+   end
+
+        it 'should not save when category is nil' do
+      @product = Product.new(
+        name:'Peach',
+        price_cents: 9999,
+        quantity: 99,
+        category: nil)
+      expect(@product).to_not be_valid
+   end
+
   end
 
 end
