b4ec4837882b09fca4b1893f640c15176e7f6ead
Rain Zhu <rain1995zhu@gmail.com>
2023-05-08 16:55:47 -0700

    set password minimum length

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence:true
   validates :last_name, presence:true
   validates :email, presence:true, uniqueness:true
-  validates :password, presence:true
+  validates :password, presence:true, length: {minimum:10}
   validates :password_confirmation, presence:true
 
   def self.authenticate_with_credentials(email, password)
