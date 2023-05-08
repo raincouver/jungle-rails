4a2098619882a1208fbc06c85999e4d38b98815c
Rain Zhu <rain1995zhu@gmail.com>
2023-05-08 15:22:08 -0700

    add and pass validate with error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe Product, type: :model do
         quantity: 99,
         category: @category)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
    end
 
         it 'should not save when price_cents is nil' do
@@ -32,6 +33,7 @@ RSpec.describe Product, type: :model do
           quantity: 99,
           category: @category)
         expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Price is not a number")
      end
 
      it 'should not save when quantity is nil' do
@@ -41,6 +43,7 @@ RSpec.describe Product, type: :model do
         quantity: nil,
         category: @category)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
    end
 
         it 'should not save when category is nil' do
@@ -50,6 +53,7 @@ RSpec.describe Product, type: :model do
         quantity: 99,
         category: nil)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
    end
 
   end
